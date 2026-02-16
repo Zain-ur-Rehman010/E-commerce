6bcbe053c306e10b6db9822cd6b8376a130737ca
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-17 04:22:50 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,9 +17,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Inhein aise likhein
     'shop',
-    'shop.cart',
-    'shop.orders',
+    'cart',   
+    'orders',
 ]
 
 MIDDLEWARE = [
