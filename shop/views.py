1d4d696d8866f021bbf1c3d91a942a3cd2c911ec
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-17 06:27:22 +0500

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -15,3 +15,11 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+def about(request):
+    return render(request, 'shop/pages/content/about.html')
+
+def blog_detail(request):
+    return render(request, 'shop/pages/content/blog.html')
+
+def contact(request):
+    return render(request, 'shop/pages/content/contact.html')
