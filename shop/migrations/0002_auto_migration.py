244110e12a21b1b24d0e8e9466d70cffd9d67ad5
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-17 05:05:07 +0500

    Update 0002_auto_migration.py

--- shop/migrations/0002_auto_migration.py
+++ shop/migrations/0002_auto_migration.py
@@ -2,26 +2,11 @@ from django.db import migrations, models
 import django.utils.timezone
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('shop', '0001_initial'), # Yaqeen kar lein ke aapki pehli file ka naam yehi hai
+        ('shop', '0001_initial'),
     ]
-
     operations = [
-        migrations.AddField(
-            model_name='product',
-            name='created',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='product',
-            name='updated',
-            field=models.DateTimeField(auto_now=True),
-        ),
-        migrations.AddField(
-            model_name='product',
-            name='stock',
-            field=models.PositiveIntegerField(default=0),
-        ),
+        migrations.AddField(model_name='product', name='created', field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now), preserve_default=False),
+        migrations.AddField(model_name='product', name='updated', field=models.DateTimeField(auto_now=True)),
+        migrations.AddField(model_name='product', name='stock', field=models.PositiveIntegerField(default=0)),
     ]
