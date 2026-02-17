5a6e8a646e9ccf50c4ef4cc690c83b716f69f82f
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-17 06:28:19 +0500

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('about/', views.about, name='about'),           # About Page
+    path('blog/', views.blog_detail, name='blog_detail'), # Blog Page
+    path('contact/', views.contact, name='contact'),     # Contact Page
 ]
