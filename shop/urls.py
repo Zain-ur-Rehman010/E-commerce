88b722876c03b42f2559249f863cd85a51f1db89
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-17 06:52:29 +0500

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # Static Content Pages
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('blog/', views.blog_detail, name='blog_detail'),
+    path('blog/', views.blog_detail, name='blog'),
     
     # Product Detail
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
