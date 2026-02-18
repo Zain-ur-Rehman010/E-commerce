6bf11e33f310e12aa5a92aa976ee627af9da9ae5
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-18 23:12:32 +0500

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -4,20 +4,26 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    # Home/Product List
-    path('', views.product_list, name='product_list'),
+    # 1. Landing Page (Jab website open ho)
+    # Yeh views.home ko call karega jo 'shop/product/index.html' render karta hai
+    path('', views.home, name='home'),
+
+    # 2. Shop Page (Jab Shop link par click ho)
+    # Yeh 'shop/pages/shop/index.html' render karega
+    path('shop/', views.product_list, name='product_list'),
     
+    # Category Filter Support
+    path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+
     # Static Content Pages
     path('about/', views.about, name='about'),
-  
     path('contact/', views.contact, name='contact'),
-    # Yahan 'blog' rakhein kyunke views mein hum 'blog' function banayenge
     path('blog/', views.blog, name='blog'),
-    
+
     # Product Detail
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-    
-    # Placeholders takay crash na ho
+
+    # Placeholders
     path('cart/', views.product_list, name='cart'), 
     path('checkout/', views.product_list, name='checkout'),
 ]
