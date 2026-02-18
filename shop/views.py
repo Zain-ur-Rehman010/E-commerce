a5798f505d6324307286dc3ecae772a09bcec5d4
uzairkhan56459-spec <uzairkhan56459@gmail.com>
2026-02-18 23:11:41 +0500

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -7,14 +7,28 @@ try:
 except ImportError:
     CartAddProductForm = None
 
+
+def home(request):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    # Aapka manga hua path: shop/templates/shop/product/index.html
+    return render(request, 'shop/product/index.html', {
+        'categories': categories,
+        'products': products
+    })
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/index.html', {
+    
+    # Aapka manga hua path: shop/templates/shop/pages/shop/index.html
+    return render(request, 'shop/pages/shop/index.html', {
         'category': category, 
         'categories': categories, 
         'products': products
@@ -33,27 +47,5 @@ def about(request):
 def contact(request):
     return render(request, 'shop/pages/content/contact.html')
 
-
 def blog(request):
     return render(request, 'shop/pages/content/blog.html')
-
-def product_list(request, category_slug=None):
-    category = None  # <--- Yeh line lazmi honi chahiye
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    
-    
-    return render(request, 'shop/pages/shop/index.html', {
-        'category': category, 
-        'categories': categories, 
-        'products': products
-    })
-
-def home(request):
-    # Agar aap Home page par bhi kuch products dikhana chahte hain:
-    products = Product.objects.filter(available=True)[:4] # Sirf pehle 4 products
-    return render(request, 'shop/index.html', {'products': products}) # Maan lijiye aapka home page shop folder ke bahar templates mein hai
