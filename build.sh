eb27c33bf227dba68c44bac37bffca3f7e4f3c9a
Zain-ur-Rehman010 <zain.ur.rehman010@gmail.com>
2026-02-20 22:42:39 +0500

    Use static images for products - permanent fix for Render

--- build.sh
+++ build.sh
@@ -11,6 +11,3 @@ python manage.py migrate
 
 # Load initial product and category data
 python manage.py loaddata shop/fixtures/initial_data.json
-
-# Download product images from Unsplash (only for products without an image)
-python manage.py populate_product_images
